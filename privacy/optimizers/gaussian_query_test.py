d9780c043e9c577c8c7e5ad85fe9b83e4e535421
cclauss <cclauss@me.com>
2019-03-08 10:35:01 +0100

    from six.moves import xrange
    __xrange()__ was removed in Python 3 in favor of a reworked version of __range()__.
    
    [flake8](http://flake8.pycqa.org) testing of https://github.com/tensorflow/privacy on Python 3.7.1
    
    $ __flake8 . --count --select=E9,F63,F72,F82 --show-source --statistics__
    ```
    ./privacy/optimizers/gaussian_query_test.py:65:16: F821 undefined name 'xrange'
          for _ in xrange(1000):
                   ^
    ./research/pate_2018/ICLR2018/rdp_bucketized.py:79:12: F821 undefined name 'xrange'
      for i in xrange(n):
               ^
    ./research/pate_2018/ICLR2018/rdp_bucketized.py:106:12: F821 undefined name 'xrange'
      for i in xrange(n):
               ^
    ./research/pate_2018/ICLR2018/rdp_bucketized.py:139:12: F821 undefined name 'xrange'
      for i in xrange(n):
               ^
    4     F821 undefined name 'xrange'
    4
    ```
    __E901,E999,F821,F822,F823__ are the "_showstopper_" [flake8](http://flake8.pycqa.org) issues that can halt the runtime with a SyntaxError, NameError, etc. These 5 are different from most other flake8 issues which are merely "style violations" -- useful for readability but they do not effect runtime safety.
    * F821: undefined name `name`
    * F822: undefined name `name` in `__all__`
    * F823: local variable name referenced before assignment
    * E901: SyntaxError or IndentationError
    * E999: SyntaxError -- failed to compile a file into an Abstract Syntax Tree

--- privacy/optimizers/gaussian_query_test.py
+++ privacy/optimizers/gaussian_query_test.py
@@ -20,6 +20,7 @@ from __future__ import print_function
 
 from absl.testing import parameterized
 import numpy as np
+from six.moves import xrange
 import tensorflow as tf
 
 from privacy.optimizers import gaussian_query
