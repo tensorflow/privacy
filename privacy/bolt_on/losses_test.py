17a1b3ea3f61672358ce3410fb9ea77d4c14945c
Chang Liu <cliu@georgianpartners.com>
2019-09-20 17:58:50 -0400

    Revert "adding new loss CategoricalCrossentropy to handle multiclass for logistic regression"
    This reverts commit 93048f298f4ca15410fd9de22323cac2826e5f2d.

--- privacy/bolt_on/losses_test.py
+++ privacy/bolt_on/losses_test.py
@@ -26,7 +26,6 @@ from tensorflow.python.framework import test_util
 from tensorflow.python.keras import keras_parameterized
 from tensorflow.python.keras.regularizers import L1L2
 from privacy.bolt_on.losses import StrongConvexBinaryCrossentropy
-from privacy.bolt_on.losses import StrongConvexCategoricalCrossentropy
 from privacy.bolt_on.losses import StrongConvexHuber
 from privacy.bolt_on.losses import StrongConvexMixin
 
@@ -62,6 +61,7 @@ class StrongConvexMixinTests(keras_parameterized.TestCase):
 
   def test_not_implemented(self, fn, args):
     """Test that the given fn's are not implemented on the mixin.
+
     Args:
       fn: fn on Mixin to test
       args: arguments to fn of Mixin
@@ -77,6 +77,7 @@ class StrongConvexMixinTests(keras_parameterized.TestCase):
   ])
   def test_return_none(self, fn, args):
     """Test that fn of Mixin returns None.
+
     Args:
       fn: fn of Mixin to test
       args: arguments to fn of Mixin
@@ -98,6 +99,7 @@ class BinaryCrossesntropyTests(keras_parameterized.TestCase):
   ])
   def test_init_params(self, reg_lambda, C, radius_constant):
     """Test initialization for given arguments.
+
     Args:
       reg_lambda: initialization value for reg_lambda arg
       C: initialization value for C arg
@@ -126,6 +128,7 @@ class BinaryCrossesntropyTests(keras_parameterized.TestCase):
   ])
   def test_bad_init_params(self, reg_lambda, C, radius_constant):
     """Test invalid domain for given params. Should return ValueError.
+
     Args:
       reg_lambda: initialization value for reg_lambda arg
       C: initialization value for C arg
@@ -148,7 +151,7 @@ class BinaryCrossesntropyTests(keras_parameterized.TestCase):
        'y_true': [1],
        'result': 10000,
       },
-      {'testcase_name': 'positive gradient positive logits',
+      {'testcase_name': 'positivee gradient positive logits',
        'logits': [10000],
        'y_true': [0],
        'result': 10000,
@@ -161,6 +164,7 @@ class BinaryCrossesntropyTests(keras_parameterized.TestCase):
   ])
   def test_calculation(self, logits, y_true, result):
     """Test the call method to ensure it returns the correct value.
+
     Args:
       logits: unscaled output of model
       y_true: label
@@ -200,6 +204,7 @@ class BinaryCrossesntropyTests(keras_parameterized.TestCase):
   ])
   def test_fns(self, init_args, fn, args, result):
     """Test that fn of BinaryCrossentropy loss returns the correct result.
+
     Args:
       init_args: init values for loss instance
       fn: the fn to test
@@ -226,6 +231,7 @@ class BinaryCrossesntropyTests(keras_parameterized.TestCase):
   ])
   def test_prints(self, init_args, fn, args, print_res):
     """Test logger warning from StrongConvexBinaryCrossentropy.
+
     Args:
       init_args: arguments to init the object with.
       fn: function to test
@@ -236,162 +242,11 @@ class BinaryCrossesntropyTests(keras_parameterized.TestCase):
       loss = StrongConvexBinaryCrossentropy(*init_args)
       if fn is not None:
         getattr(loss, fn, lambda *arguments: print('error'))(*args)
-    self.assertRegexMatch(out.getvalue().strip(), [print_res])
+    self.assertRegexMatch(err.getvalue().strip(), [print_res])
 
-class CategoricalCrossesntropyTests(keras_parameterized.TestCase):
-  """tests for CategoricalCrossesntropy StrongConvex loss."""
-
-  @parameterized.named_parameters([
-      {'testcase_name': 'normal',
-       'reg_lambda': 1,
-       'C': 1,
-       'radius_constant': 1
-      },  # pylint: disable=invalid-name
-  ])
-  def test_init_params(self, reg_lambda, C, radius_constant):
-    """Test initialization for given arguments.
-    Args:
-      reg_lambda: initialization value for reg_lambda arg
-      C: initialization value for C arg
-      radius_constant: initialization value for radius_constant arg
-    """
-    # test valid domains for each variable
-    loss = StrongConvexCategoricalCrossentropy(reg_lambda, C, radius_constant)
-    self.assertIsInstance(loss, StrongConvexCategoricalCrossentropy)
-
-  @parameterized.named_parameters([
-      {'testcase_name': 'negative c',
-       'reg_lambda': 1,
-       'C': -1,
-       'radius_constant': 1
-      },
-      {'testcase_name': 'negative radius',
-       'reg_lambda': 1,
-       'C': 1,
-       'radius_constant': -1
-      },
-      {'testcase_name': 'negative lambda',
-       'reg_lambda': -1,
-       'C': 1,
-       'radius_constant': 1
-      },  # pylint: disable=invalid-name
-  ])
-  def test_bad_init_params(self, reg_lambda, C, radius_constant):
-    """Test invalid domain for given params. Should return ValueError.
-    Args:
-      reg_lambda: initialization value for reg_lambda arg
-      C: initialization value for C arg
-      radius_constant: initialization value for radius_constant arg
-    """
-    # test valid domains for each variable
-    with self.assertRaises(ValueError):
-      StrongConvexCategoricalCrossentropy(reg_lambda, C, radius_constant)
-
-  @test_util.run_all_in_graph_and_eager_modes
-  @parameterized.named_parameters([
-      # [] for compatibility with tensorflow loss calculation
-      {'testcase_name': 'both positive',
-       'logits': [10000, 0],
-       'y_true': [1, 0],
-       'result': 0,
-      },
-      {'testcase_name': 'positive gradient negative logits',
-       'logits': [-10000, 0],
-       'y_true': [1, 0],
-       'result': 10000,
-      },
-      {'testcase_name': 'positive gradient positive logits',
-       'logits': [10000, 0],
-       'y_true': [0, 1],
-       'result': 10000,
-      },
-      {'testcase_name': 'both negative',
-       'logits': [-10000, 0],
-       'y_true': [0, 1],
-       'result': 0
-      },
-  ])
-  def test_calculation(self, logits, y_true, result):
-    """Test the call method to ensure it returns the correct value.
-    Args:
-      logits: unscaled output of model
-      y_true: label
-      result: correct loss calculation value
-    """
-    logits = tf.Variable(logits, False, dtype=tf.float32)
-    y_true = tf.Variable(y_true, False, dtype=tf.float32)
-    loss = StrongConvexCategoricalCrossentropy(0.00001, 1, 1)
-    loss = loss(y_true, logits)
-    self.assertEqual(loss.numpy(), result)
-
-  @parameterized.named_parameters([
-      {'testcase_name': 'beta',
-       'init_args': [1, 1, 1],
-       'fn': 'beta',
-       'args': [1],
-       'result': tf.constant(2, dtype=tf.float32)
-      },
-      {'testcase_name': 'gamma',
-       'fn': 'gamma',
-       'init_args': [1, 1, 1],
-       'args': [],
-       'result': tf.constant(1, dtype=tf.float32),
-      },
-      {'testcase_name': 'lipchitz constant',
-       'fn': 'lipchitz_constant',
-       'init_args': [1, 1, 1],
-       'args': [1],
-       'result': tf.constant(2, dtype=tf.float32),
-      },
-      {'testcase_name': 'kernel regularizer',
-       'fn': 'kernel_regularizer',
-       'init_args': [1, 1, 1],
-       'args': [],
-       'result': L1L2(l2=0.5),
-      },
-  ])
-  def test_fns(self, init_args, fn, args, result):
-    """Test that fn of CategoricalCrossentropy loss returns the correct result.
-    Args:
-      init_args: init values for loss instance
-      fn: the fn to test
-      args: the arguments to above function
-      result: the correct result from the fn
-    """
-    loss = StrongConvexCategoricalCrossentropy(*init_args)
-    expected = getattr(loss, fn, lambda: 'fn not found')(*args)
-    if hasattr(expected, 'numpy') and hasattr(result, 'numpy'):  # both tensor
-      expected = expected.numpy()
-      result = result.numpy()
-    if hasattr(expected, 'l2') and hasattr(result, 'l2'):  # both l2 regularizer
-      expected = expected.l2
-      result = result.l2
-    self.assertEqual(expected, result)
-
-  @parameterized.named_parameters([
-      {'testcase_name': 'label_smoothing',
-       'init_args': [1, 1, 1, True, 0.1],
-       'fn': None,
-       'args': None,
-       'print_res': 'The impact of label smoothing on privacy is unknown.'
-      },
-  ])
-  def test_prints(self, init_args, fn, args, print_res):
-    """Test logger warning from StrongConvexCategoricalCrossentropy.
-    Args:
-      init_args: arguments to init the object with.
-      fn: function to test
-      args: arguments to above function
-      print_res: print result that should have been printed.
-    """
-    with captured_output() as (out, err):  # pylint: disable=unused-variable
-      loss = StrongConvexCategoricalCrossentropy(*init_args)
-      if fn is not None:
-        getattr(loss, fn, lambda *arguments: print('error'))(*args)
-    self.assertRegexMatch(out.getvalue().strip(), [print_res])
 
 class HuberTests(keras_parameterized.TestCase):
-  """tests for CategoricalCrossesntropy StrongConvex loss."""
+  """tests for BinaryCrossesntropy StrongConvex loss."""
 
   @parameterized.named_parameters([
       {'testcase_name': 'normal',
@@ -403,6 +258,7 @@ class HuberTests(keras_parameterized.TestCase):
   ])
   def test_init_params(self, reg_lambda, c, radius_constant, delta):
     """Test initialization for given arguments.
+
     Args:
       reg_lambda: initialization value for reg_lambda arg
       c: initialization value for C arg
@@ -441,6 +297,7 @@ class HuberTests(keras_parameterized.TestCase):
   ])
   def test_bad_init_params(self, reg_lambda, c, radius_constant, delta):
     """Test invalid domain for given params. Should return ValueError.
+
     Args:
       reg_lambda: initialization value for reg_lambda arg
       c: initialization value for C arg
@@ -511,6 +368,7 @@ class HuberTests(keras_parameterized.TestCase):
   ])
   def test_calculation(self, logits, y_true, delta, result):
     """Test the call method to ensure it returns the correct value.
+
     Args:
       logits: unscaled output of model
       y_true: label
@@ -551,6 +409,7 @@ class HuberTests(keras_parameterized.TestCase):
   ])
   def test_fns(self, init_args, fn, args, result):
     """Test that fn of BinaryCrossentropy loss returns the correct result.
+
     Args:
       init_args: init values for loss instance
       fn: the fn to test
