e002cc9a239b3c6503095ac30b2c93f50f6b0274
A. Unique TensorFlower <gardener@tensorflow.org>
2020-03-09 15:35:27 -0700

    Automated refactoring to make code Python 3 compatible.
    PiperOrigin-RevId: 299951850

--- tensorflow_privacy/privacy/optimizers/dp_optimizer_eager_test.py
+++ tensorflow_privacy/privacy/optimizers/dp_optimizer_eager_test.py
@@ -19,6 +19,7 @@ from __future__ import print_function
 
 from absl.testing import parameterized
 import numpy as np
+from six.moves import range
 import tensorflow.compat.v1 as tf
 
 from tensorflow_privacy.privacy.analysis import privacy_ledger
