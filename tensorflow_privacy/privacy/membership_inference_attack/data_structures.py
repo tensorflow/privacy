13d1676a005bfda031c634801c33423d872a331d
Liwei Song <liweisongpku@gmail.com>
2020-12-10 18:20:32 -0500

    edit the summary string for privacy risk scores

--- tensorflow_privacy/privacy/membership_inference_attack/data_structures.py
+++ tensorflow_privacy/privacy/membership_inference_attack/data_structures.py
@@ -566,7 +566,7 @@ class SingleRiskScoreResult:
     """
     meaningful_threshold_list, precision_list, recall_list = self.attack_with_varied_thresholds(threshold_list)
     summary = []
-    summary.append('Privacy risk score analysis over slice: \"%s\"' %
+    summary.append('\nPrivacy risk score analysis over slice: \"%s\"' %
                    str(self.slice_spec))
     for i in range(len(meaningful_threshold_list)):
       summary.append('  with %.5f as the threshold on privacy risk score, the precision-recall pair is (%.5f, %.5f)' %
