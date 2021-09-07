06eef5136908961ea5a235341ec88c2b9838e85b
Galen Andrew <galenandrew@google.com>
2021-09-07 09:23:38 -0700

    New version 0.7.3
    PiperOrigin-RevId: 395263672

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import setup
 
 setup(
     name='tensorflow_privacy',
-    version='0.7.2',
+    version='0.7.3',
     url='https://github.com/tensorflow/privacy',
     license='Apache-2.0',
     install_requires=[
--- tensorflow_privacy/version.py
+++ tensorflow_privacy/version.py
@@ -13,4 +13,4 @@
 # limitations under the License.
 """TensorFlow Privacy version."""
 
-__version__ = '0.7.2'
+__version__ = '0.7.3'
