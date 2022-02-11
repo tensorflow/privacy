592ed995f1f6272e7087ba1b67d101525ef54192
Michael Reneer <michaelreneer@google.com>
2022-02-10 21:19:29 -0800

    Update the direct `scipy` dependency to `~=1.2.1`.
    This means that we would accept a hot fix, but not accept major or minor version changes without releasing TFP.
    
    PiperOrigin-RevId: 427916084

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
         'matplotlib~=3.3.4',
         'numpy~=1.21.5',
         'pandas~=1.1.4',
-        'scipy>=0.17',
+        'scipy~=1.2.1',
         'tensorflow-datasets>=4.4.0',
         'tensorflow-estimator>=2.3.0',
         'tensorflow-probability>=0.13.0',
