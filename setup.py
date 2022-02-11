f393322593655f0f4b7b8f8c6b61f5739f293f9a
Michael Reneer <michaelreneer@google.com>
2022-02-11 09:37:04 -0800

    Automated rollback of commit 823aacb315afb79a72a3e374c944587c9a261bdd
    PiperOrigin-RevId: 428022174

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ setup(
         'sklearn~=1.0.2',
         'tensorflow-datasets>=4.4.0',
         'tensorflow-estimator>=2.3.0',
+        'tensorflow-probability>=0.13.0',
     ],
     # Explicit dependence on TensorFlow is not supported.
     # See https://github.com/tensorflow/tensorflow/issues/7166
