60d237be83976dcb7a294d76b58b002b07655672
Steve Chien <schien@google.com>
2023-05-19 14:21:44 -0700

    Update tensorflow-probability version to 0.20.0
    PiperOrigin-RevId: 533550592

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
         'statsmodels~=0.13',
         'tensorflow-datasets~=4.5',
         'tensorflow-estimator~=2.4',
-        'tensorflow-probability==0.15.0',
+        'tensorflow-probability~=0.20.0',
         'tensorflow~=2.4',
     ],
     packages=find_packages(),
