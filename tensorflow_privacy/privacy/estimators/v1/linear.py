134c898ded0e6ad351738421b27cd65bb0cb4060
Steve Chien <schien@google.com>
2023-07-26 16:38:09 -0700

    Add DP-SGD version of v1 LinearClassifier.
    PiperOrigin-RevId: 551350685

--- tensorflow_privacy/privacy/estimators/v1/linear.py
+++ tensorflow_privacy/privacy/estimators/v1/linear.py
@@ -1,4 +1,4 @@
-# Copyright 2020 The TensorFlow Authors. All Rights Reserved.
+# Copyright 2023 The TensorFlow Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,58 +12,61 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""DP version of DNNClassifiers v1."""
+"""DP version of LinearClassifier v1."""
 
 import tensorflow as tf
-
 from tensorflow_privacy.privacy.estimators.v1 import head as head_lib
-from tensorflow_estimator.python.estimator import estimator
-from tensorflow_estimator.python.estimator.canned import dnn
+from tensorflow_estimator.python.estimator import estimator  # pylint: disable=g-deprecated-tf-checker
+from tensorflow_estimator.python.estimator.canned import linear  # pylint: disable=g-deprecated-tf-checker
 
 
-class DNNClassifier(estimator.Estimator):
-  """DP version of `tf.compat.v1.estimator.DNNClassifier`."""
+class LinearClassifier(estimator.Estimator):
+  """DP version of `tf.compat.v1.estimator.LinearClassifier`."""
 
   def __init__(
       self,
-      hidden_units,
       feature_columns,
       model_dir=None,
       n_classes=2,
       weight_column=None,
       label_vocabulary=None,
-      optimizer='Adagrad',
-      activation_fn=tf.nn.relu,
-      dropout=None,
-      input_layer_partitioner=None,
+      optimizer='Ftrl',
       config=None,
+      partitioner=None,
       warm_start_from=None,
       loss_reduction=tf.compat.v1.losses.Reduction.SUM,  # For scalar summary.
-      batch_norm=False,
+      sparse_combiner='sum',
   ):
-    """See `tf.compat.v1.estimator.DNNClassifier`."""
+    """See `tf.compat.v1.estimator.LinearClassifier`."""
+    linear._validate_linear_sdca_optimizer_for_linear_classifier(  # pylint: disable=protected-access
+        feature_columns=feature_columns,
+        n_classes=n_classes,
+        optimizer=optimizer,
+        sparse_combiner=sparse_combiner,
+    )
+    estimator._canned_estimator_api_gauge.get_cell('Classifier').set('Linear')  # pylint: disable=protected-access
+
     head = head_lib._binary_logistic_or_multi_class_head(  # pylint: disable=protected-access
-        n_classes, weight_column, label_vocabulary, loss_reduction)
-    estimator._canned_estimator_api_gauge.get_cell('Classifier').set('DNN')
+        n_classes, weight_column, label_vocabulary, loss_reduction
+    )
 
     def _model_fn(features, labels, mode, config):
-      """Call the defined shared dnn_model_fn."""
-      return dnn._dnn_model_fn(  # pylint: disable=protected-access
+      """Call the defined shared _linear_model_fn."""
+      return linear._linear_model_fn(  # pylint: disable=protected-access
           features=features,
           labels=labels,
           mode=mode,
           head=head,
-          hidden_units=hidden_units,
           feature_columns=tuple(feature_columns or []),
           optimizer=optimizer,
-          activation_fn=activation_fn,
-          dropout=dropout,
-          input_layer_partitioner=input_layer_partitioner,
+          partitioner=partitioner,
           config=config,
-          batch_norm=batch_norm)
+          sparse_combiner=sparse_combiner,
+      )
 
-    super().__init__(
+    super(LinearClassifier, self).__init__(
         model_fn=_model_fn,
         model_dir=model_dir,
         config=config,
-        warm_start_from=warm_start_from)
+        warm_start_from=warm_start_from,
+    )
